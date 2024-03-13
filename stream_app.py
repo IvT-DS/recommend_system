e30ac9f8aa1e00a6675a13ee846cd0ec6233ae0a
IvT-DS <i.s.tereshchenko@gmail.com>
2024-03-13 21:16:33 +0600

    upd req

--- stream_app.py
+++ stream_app.py
@@ -26,15 +26,23 @@ if st.button("Выбрать случайно"):
                 f"<h5 style='font-weight:bold;'>Название сериала: «{df['title'][i]}»</h5>",
                 unsafe_allow_html=True,
             )
+
             st.markdown(
-                "<h6 style='font-weight:bold;'>Описание:</h6>", unsafe_allow_html=True
+                f"<h5 style='font-weight:bold;'>Год выпуска: «{df['title'][i]}»</h5>",
+                unsafe_allow_html=True,
             )
-            st.write(df["description"][i])
+
             genre = f"Нет данных" if pd.isna(df["ganres"].iloc[i]) else df["ganres"][i]
             st.markdown(
                 f"<h6 style='font-weight:bold;'>Жанр: {genre}</h6>",
                 unsafe_allow_html=True,
             )
+
+            st.markdown(
+                "<h6 style='font-weight:bold;'>Описание:</h6>", unsafe_allow_html=True
+            )
+            st.write(df["description"][i])
+
             # st.write("Нет данных" if pd.isna(df['genres'].iloc[i]) else df['genres'][i] )
             # st.title('Оценка')
             # imb = 'Нет оценки' if df['imdb'][i] == 0 else str(df['imdb'][i])
