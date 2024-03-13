98e09e0ee845c7587aa03f1e699af310ceb7a114
IvT-DS <i.s.tereshchenko@gmail.com>
2024-03-14 01:07:31 +0600

    upd stream

--- stream_app.py
+++ stream_app.py
@@ -21,13 +21,29 @@ if st.button("Выбрать случайно"):
             st.image(df["poster"][i])
 
         with col2:
-            genre = f"Нет данных" if pd.isna(df["genres"].iloc[i]) else df["genres"][i]
+            
+
+            # Заголовки полей жирным шрифтом
+            st.markdown(
+                "<h4 style='font-weight:bold;'>Название сериала:</h4>",
+                unsafe_allow_html=True,
+            )
+            # Данные обычным текстом
+            st.write(f"«{df['title'][i]}»")
 
             st.markdown(
-                f"<h4 style='font-weight:bold;display:inline;'>Название сериала:</h4> {df['title'][i]}"
-                f"<br><h6 style='font-weight:bold;display:inline;'>Страна:</h6> {df['country'][i]}"
-                f"<br><h6 style='font-weight:bold;display:inline;'>Год выпуска:</h6> {df['year1'][i]}"
-                f"<br><h6 style='font-weight:bold;display:inline;'>Жанр:</h6> {genre}",
+                f"<h6 style='font-weight:bold;'>Страна: «{df['country'][i]}»</h6>",
+                unsafe_allow_html=True,
+            )
+
+            st.markdown(
+                f"<h6 style='font-weight:bold;'>Год выпуска: {df['year1'][i]}</h6>",
+                unsafe_allow_html=True,
+            )
+
+            genre = f"Нет данных" if pd.isna(df["genres"].iloc[i]) else df["genres"][i]
+            st.markdown(
+                f"<h6 style='font-weight:bold;'>Жанр: {genre}</h6>",
                 unsafe_allow_html=True,
             )
 
@@ -41,10 +57,5 @@ if st.button("Выбрать случайно"):
             )
             st.write(df["description"][i])
 
-            # st.write("Нет данных" if pd.isna(df['genres'].iloc[i]) else df['genres'][i] )
-            # st.title('Оценка')
-            # imb = 'Нет оценки' if df['imdb'][i] == 0 else str(df['imdb'][i])
-            # kinopoisk = 'Нет оценки' if df['kinopoisk'][i] == 0 else str(df['kinopoisk'][i])
-            # st.write(f'Рейтинг imdb: {imb}')
-            # st.write(f'Рейтинг кинопоиск: {kinopoisk}')
+           
         st.markdown("---")
